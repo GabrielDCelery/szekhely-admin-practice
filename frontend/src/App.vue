428b9fefe8b78bf47825e32d43a7581137d567f7
Gabriel Celery <gabriel.celery@autologyx.com>
2018-11-16 09:41:55 +0000

    - Added Vuex store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <v-toolbar>
+      <v-toolbar-items>
+        <v-btn flat>Companies</v-btn>
+        <v-btn flat>Mailing</v-btn>
+        <v-btn flat>Invoices</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -15,6 +22,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
 </style>
